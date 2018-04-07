be4a0c691744219621aa8a4b1f16c5bd5631c2a7
Edmene <cristianedsonm.com>
2018-04-07 10:54:08 -0300

    Updated migration file with new DDL modifications due to changes in ER model.

--- codificacao/src/migrations/20180328301818_create_tables.sql
+++ codificacao/src/migrations/20180328301818_create_tables.sql
@@ -27,7 +27,8 @@ CREATE TABLE users(
   id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(150) NOT NULL,
   password VARCHAR(600) NOT NULL,
-  extra VARCHAR(200) NOT NULL
+  extra VARCHAR(200) NOT NULL,
+  type CHAR(1) NOT NULL
 );
 
 CREATE TABLE passengers (
@@ -68,17 +69,56 @@ CREATE TABLE plans(
   availability_condition CHAR(1) NOT NULL,
   ticket_price FLOAT CHECK (ticket_price > 0),
   daily_value FLOAT CHECK (daily_value > 0),
-  available_reservations SMALLINT NOT NULL CHECK (plans.available_reservations > 0),
-  destination_id INT REFERENCES destinations NOT NULL
+  available_reservations SMALLINT NOT NULL CHECK (plans.available_reservations > 0)
 );
 
 CREATE TABLE availabilities(
-  shift INT NOT NULL,
   day INT NOT NULL,
+  shift INT NOT NULL,
   plan_id INT NOT NULL REFERENCES plans,
-  PRIMARY KEY (shift, day, plan_id)
+  driver_id INT NOT NULL REFERENCES drivers,
+  vehicle_id INT NOT NULL REFERENCES vehicles,
+  PRIMARY KEY (day, shift, plan_id, driver_id, vehicle_id)
 );
 
+CREATE TABLE stops(
+  id SERIAL NOT NULL PRIMARY KEY,
+  time TIME NOT NULL,
+  address_id INT NOT NULL REFERENCES addresses,
+  day INT NOT NULL,
+  shift INT NOT NULL,
+  plan_id INT NOT NULL,
+  driver_id INT NOT NULL,
+  vehicle_id INT NOT NULL,
+  CONSTRAINT availabilities_fk FOREIGN KEY (day, shift, plan_id, driver_id, vehicle_id)
+  REFERENCES availabilities(day, shift, plan_id, driver_id, vehicle_id)
+);
+
+CREATE TABLE reservations(
+  reservation_type CHAR(1) NOT NULL,
+  status BOOLEAN NOT NULL,
+  date DATE,
+  alteration_date DATE,
+  passenger_id INT NOT NULL REFERENCES passengers,
+  stop_id INT NOT NULL REFERENCES stops
+);
+
+CREATE TABLE passenger_plans (
+  passenger_id INT NOT NULL REFERENCES passengers,
+  destination_id INT NOT NULL,
+  plan_id INT NOT NULL,
+  CONSTRAINT destination_plans_fk FOREIGN KEY (destination_id, plan_id)
+  REFERENCES destination_plans(destination_id, plan_id),
+  PRIMARY KEY(passenger_id, destination_id, plan_id)
+);
+
+CREATE TABLE destination_plans (
+  destination_id INT NOT NULL REFERENCES destinations,
+  plan_id INT NOT NULL REFERENCES plans,
+  PRIMARY KEY (destination_id, plan_id)
+);
+
+/*
 CREATE TABLE allocations(
   driver_id INT NOT NULL REFERENCES drivers,
   shift INT NOT NULL,
@@ -99,23 +139,4 @@ CREATE TABLE vehicle_allocations(
   REFERENCES allocations(driver_id, shift, day, plan_id),
   PRIMARY KEY (vehicle_id, driver_id, shift, day, plan_id)
 );
-
-CREATE TABLE stops(
-  id SERIAL NOT NULL PRIMARY KEY,
-  time TIME NOT NULL,
-  address_id INT NOT NULL REFERENCES addresses,
-  shift INT NOT NULL,
-  day INT NOT NULL,
-  plan_id INT NOT NULL,
-  CONSTRAINT availabilities_fk FOREIGN KEY (shift, day, plan_id)
-  REFERENCES availabilities(shift, day, plan_id)
-);
-
-CREATE TABLE reservations(
-  reservation_type CHAR(1) NOT NULL,
-  status BOOLEAN NOT NULL,
-  date DATE NOT NULL,
-  alteration_date DATE,
-  passenger_id INT NOT NULL REFERENCES passengers,
-  stop_id INT NOT NULL REFERENCES stops
-);
+*/
