f5aa56f76afd60b37a946a47a3bc87ce483dfc1a
Cristian E. Meneghetti <cristianedsonm.com>
2018-04-13 11:08:52 -0300

    Destination_Plan model updated

--- codificacao/src/main/java/app/models/DestinationPlan.java
+++ codificacao/src/main/java/app/models/DestinationPlan.java
@@ -1,11 +1,14 @@
 package app.models;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.CompositePK;
 import org.javalite.activejdbc.annotations.Table;
 
 @Table("destination_plans")
+@CompositePK({ "destination_id", "plan_id" })
 public class DestinationPlan extends Model {
     static {
-        validatePresenceOf("destination_id","plan_id");
+        validatePresenceOf("destination_id","plan_id")
+                .message("Falta um ou mais chaves primárias");
     }
 }
