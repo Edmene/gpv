d253f4362de1d2953f8a80f10a725ed81952f71d
Cristian E. Meneghetti <cristianedsonm.com>
2018-06-15 10:58:44 -0300

    Modifications.

--- codificacao/src/main/java/app/utils/TransformMaskeredInput.java
+++ codificacao/src/main/java/app/utils/TransformMaskeredInput.java
@@ -1,7 +1,11 @@
 package app.utils;
 
+import java.util.ArrayList;
+
 public class TransformMaskeredInput {
-    private static char[] filteredCharacters = {'.',',','-','(',')',' '};
+    private static char[] filteredCharacters = {'.',',','-','(',')',' '
+            ,'"','!','$','#','%','&','*','_','+','=','[',']','^','`',
+    ':','\\','/'};
     public static String format(String input){
         for(char character : filteredCharacters){
             input=input.replaceAll(String.valueOf("\\"+character),"");
@@ -9,9 +13,27 @@ public class TransformMaskeredInput {
         return input;
     }
 
+    public static String format(String input, ArrayList<String> skipCheck){
+        for(char character : filteredCharacters){
+            if(!skipCheck.contains(String.valueOf(character))) {
+                input = input.replaceAll(String.valueOf("\\" + character), "");
+            }
+        }
+        return input;
+    }
+
     public static String format(String input, String replacement){
         for(char character : filteredCharacters){
             input=input.replaceAll(String.valueOf("\\"+character),replacement);
+        }
+        return input;
+    }
+
+    public static String format(String input, String replacement, ArrayList<String> skipCheck){
+        for(char character : filteredCharacters){
+            if(!skipCheck.contains(String.valueOf(character))) {
+                input = input.replaceAll(String.valueOf("\\" + character), replacement);
+            }
         }
         return input;
     }
