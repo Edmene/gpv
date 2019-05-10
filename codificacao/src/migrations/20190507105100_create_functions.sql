4811ecf18af512b598d23bf207e9bea34bfbfffe
Cristian E. Meneghetti <cristianedsonm.com>
2019-05-10 11:20:03 -0300

    Passenger CRUD in progress: addition of preliminary function for its insertion.

--- codificacao/src/migrations/20190507105100_create_functions.sql
+++ codificacao/src/migrations/20190507105100_create_functions.sql
@@ -1,3 +1,15 @@
-CREATE OR REPLACE FUNCTION passenger_creation() AS
+CREATE OR REPLACE FUNCTION passenger_creation(sys_user users%rowtype, passenger passengers%rowtype) AS
         $$
+        declare
+            user_id integer;
+        begin
+            INSERT INTO users(name,password,extra,type) VALUES (sys_user)
+                RETURNING id INTO user_id;
+
+            passenger.user_id = user_id;
+
+            INSERT into passengers(user_id,name,surname,cpf,rg,
+                                    birth_date,telephone,email)
+                                    VALUES (passenger);
+        end;
     $$;
