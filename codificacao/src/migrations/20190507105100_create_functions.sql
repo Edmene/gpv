315c1bd2c08b27304c17a5f53aca6b005c7d6f40
Edmene <cristianedsonm.com>
2019-05-12 15:29:38 -0300

    Fixed declaration of function creation in migration.

--- codificacao/src/migrations/20190507105100_create_functions.sql
+++ codificacao/src/migrations/20190507105100_create_functions.sql
@@ -1,15 +1,29 @@
-CREATE OR REPLACE FUNCTION passenger_creation(sys_user users%rowtype, passenger passengers%rowtype) AS
-        $$
-        declare
-            user_id integer;
-        begin
-            INSERT INTO users(name,password,extra,type) VALUES (sys_user)
-                RETURNING id INTO user_id;
+DELIMITER '
+'
+CREATE OR REPLACE FUNCTION passenger_creation(sys_user users, passenger passengers) RETURNS BOOLEAN
+    AS $$
+declare
 
-            passenger.user_id = user_id;
+    user_id integer;
+begin
+INSERT INTO users(name,password,extra,type) VALUES (sys_user.name,
+                                                    sys_user.password,
+                                                    sys_user.extra,
+                                                    sys_user.type)
+                                                   RETURNING id INTO user_id;
 
-            INSERT into passengers(user_id,name,surname,cpf,rg,
-                                    birth_date,telephone,email)
-                                    VALUES (passenger);
-        end;
-    $$;
+passenger.user_id = user_id;
+
+INSERT into passengers(user_id,name,surname,cpf,rg,
+                       birth_date,telephone,email)
+VALUES (passenger.user_id,
+        passenger.name,
+        passenger.surname,
+        passenger.cpf,
+        passenger.rg,
+        passenger.birth_date,
+        passenger.telephone,
+        passenger.email);
+return true;
+end
+    $$ language plpgsql;'
