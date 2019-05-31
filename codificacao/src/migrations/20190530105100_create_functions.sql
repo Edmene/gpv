c60f5aefc1f4143e62f84fca4c952fd63e8cbf1f
Edmene <cristianedsonm.com>
2019-05-30 21:44:39 -0300

    Changes in passenger creation function.

---
+++ codificacao/src/migrations/20190530105100_create_functions.sql
@@ -0,0 +1,42 @@
+DELIMITER '
+'
+CREATE OR REPLACE FUNCTION passenger_creation(user_name users.name%type, secret users.password%type,
+                                              u_extra users.extra%type, p_name passengers.name%type, p_surname passengers.surname%type,
+                                              p_cpf passengers.cpf%type, p_rg passengers.rg%type, p_telephone passengers.telephone%type,
+                                              p_email passengers.email%type, p_birth_date passengers.birth_date%type) RETURNS BOOLEAN
+AS $$
+declare
+
+    userId integer;
+begin
+
+    begin
+        INSERT INTO users(name,password,extra,type) VALUES (user_name,
+                                                            secret,
+                                                            u_extra,
+                                                            'P')
+                                                           RETURNING id INTO userId;
+    exception
+        when unique_violation then
+            return false;
+    end;
+
+    begin
+        INSERT into passengers(user_id,name,surname,cpf,rg,
+                               birth_date,telephone,email)
+        VALUES (userId,
+                p_name,
+                p_surname,
+                p_cpf,
+                p_rg,
+                p_birth_date,
+                p_telephone,
+                p_email);
+        return true;
+    exception
+        when unique_violation then
+            DELETE FROM users WHERE id = userId;
+            return false;
+    end;
+end
+$$ language plpgsql;'
