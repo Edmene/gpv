102cb407ecb2aab2c9f4e28b67b0d826be1beafe
Edmene <cristianedsonm.com>
2019-06-04 20:00:25 -0300

    DriverVehicleReplacement has its methods improved.

--- codificacao/src/main/java/app/controllers/DriverVehicleReplacementController.java
+++ codificacao/src/main/java/app/controllers/DriverVehicleReplacementController.java
@@ -8,9 +8,18 @@ import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.LazyList;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DriverVehicleReplacementController extends GenericAppController {
+
+    private boolean isEqualDriverVehicleReplacement(DriverVehicleReplacement driverVehicler, DriverVehicleReplacementJson driverVehiclerJson){
+        return (driverVehicler.getInteger("driver_id") == driverVehiclerJson.driverKey &&
+                driverVehicler.getInteger("vehicle_id") == driverVehiclerJson.vehicleKey &&
+                driverVehicler.getInteger("driver_v_id") == driverVehiclerJson.driverVKey &&
+                driverVehicler.getInteger("vehicle_v_id") == driverVehiclerJson.vehicleVKey &&
+                driverVehicler.getDate("date").toLocalDate() == driverVehiclerJson.date);
+    }
 
     private ArrayList<DriverVehicleReplacementJson>
     driverVehicleReplacementsToDriverVehicleReplacementJsonList(LazyList<DriverVehicleReplacement> driverVehicleReplacements){
@@ -83,19 +92,28 @@ public class DriverVehicleReplacementController extends GenericAppController {
         }
     }
 
-    @Override
-    public void update(@NotNull Context ctx, @NotNull String resourceId){
+    public void update(@NotNull Context ctx, @NotNull String resourceId, @NotNull String vehicleId,
+                       @NotNull String driverVId, @NotNull String vehicleVId){
         try {
+            int status = 400;
             Base.open(Db.getInstance());
-            DriverVehicleReplacement driverVehicleReplacement = new DriverVehicleReplacement();
             DriverVehicleReplacementJson driverVehicleReplacementJson = ctx.bodyAsClass(DriverVehicleReplacementJson.class);
-            driverVehicleReplacementJson.setAttributesOfDriverVehicleReplacement(driverVehicleReplacement);
-            if(driverVehicleReplacement.save()){
-                ctx.res.setStatus(200);
+            DriverVehicleReplacement driverVehicleReplacement = DriverVehicleReplacement.findByCompositeKeys(
+                    Integer.parseInt(resourceId),Integer.parseInt(vehicleId),
+                    Integer.parseInt(driverVId),Integer.parseInt(vehicleVId)
+            );
+            if(driverVehicleReplacement == null || driverVehicleReplacementJson.date == null){
+                status = 404;
             }
-            else{
-                ctx.res.setStatus(400);
+            else {
+                if(isEqualDriverVehicleReplacement(driverVehicleReplacement, driverVehicleReplacementJson)) {
+                    driverVehicleReplacementJson.setAttributesOfDriverVehicleReplacement(driverVehicleReplacement);
+                    if (driverVehicleReplacement.save()) {
+                        status = 200;
+                    }
+                }
             }
+            ctx.res.setStatus(status);
             Base.close();
         }
         catch (Exception e){
@@ -105,11 +123,14 @@ public class DriverVehicleReplacementController extends GenericAppController {
         }
     }
 
-    @Override
-    public void delete(@NotNull Context ctx, @NotNull String resourceId){
+    public void delete(@NotNull Context ctx, @NotNull String resourceId, @NotNull String vehicleId,
+                       @NotNull String driverVId, @NotNull String vehicleVId){
         try{
             Base.open(Db.getInstance());
-            DriverVehicleReplacement driverVehicleReplacement = DriverVehicleReplacement.findById(Integer.parseInt(resourceId));
+            DriverVehicleReplacement driverVehicleReplacement = DriverVehicleReplacement.findByCompositeKeys(
+                    Integer.parseInt(resourceId),Integer.parseInt(vehicleId),
+                    Integer.parseInt(driverVId),Integer.parseInt(vehicleVId)
+            );
             if(driverVehicleReplacement == null){
                 ctx.res.setStatus(404);
             }
